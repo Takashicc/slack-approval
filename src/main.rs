5084129742fc13609e88a6c1e98bda237ab1ea17
Takashicc <105186894+Takashicc@users.noreply.github.com>
2024-12-23 23:01:51 +0900

    feat: Add base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use anyhow::Result;
+use tracing::{error, info};
+
+mod services;
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt().init();
+    info!("Start");
+    if let Err(e) = execute() {
+        error!("{:?}", e);
+        return;
+    }
+    info!("End")
+}
+
+fn execute() -> Result<()> {
+    Ok(())
 }
